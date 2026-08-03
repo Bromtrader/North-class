ddb56a7c2f84537896d826d23f810ebfba3bdb80
B R O M ⌬ 天使 <168416927+Bromtrader@users.noreply.github.com>
2026-08-03 21:40:19 +0300

    Update timetable.js

--- data/timetable.js
+++ data/timetable.js
@@ -22,7 +22,7 @@
  * EXAMPLE:
  *
  * { day: "Monday", subject: "Mathematics", teacher: "Mr. Bello",
- *   room: "Room 4", start: "08:00", end: "08:40" },
+    room: "Room 4", start: "08:00", end: "08:40" },
  */
 
 const TIMETABLE = {
@@ -30,7 +30,16 @@ const TIMETABLE = {
   days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
 
   periods: [
-
+{ day: "Monday", subject: "Mathematics", teacher: "Mr. BROM",
+    room: "Room 4", start: "08:00", end: "08:40" },
+{ day: "Tuesday", subject: "Chemistry", teacher: "Mr. WUGO",
+    room: "Room 3", start: "08:40", end: "09:20" },
+{ day: "Wednesday", subject: "Computer", teacher: "OBVIOUSLY BROM",
+    room: "Room 1", start: "09:20", end: "10:00" },
+{ day: "Thursdsay", subject: "P.E", teacher: "CHAIRMAN",
+    room: "Room 6", start: "10:00", end: "10:20" },
+{ day: "Friday", subject: "P.E", teacher: "CHAIRMAN",
+    room: "Room 5", start: "10:20", end: "11:00" },
   ]
 
 };
